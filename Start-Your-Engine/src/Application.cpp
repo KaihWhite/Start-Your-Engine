72c849f6ec9695a376b88aed1e1dcba92e26879b
Knochesv2 <136147811+Knochesv2@users.noreply.github.com>
2023-10-25 20:17:58 -0700

    Add files via upload
    sprint 2 commit

--- Start-Your-Engine/src/Application.cpp
+++ Start-Your-Engine/src/Application.cpp
@@ -1,40 +1,25 @@
-#pragma once
-
 #include <iostream>
-#include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
-// Callback function for resizing the window
-void framebuffer_size_callback(GLFWwindow* window, int width, int height)
-{
-    glViewport(0, 0, width, height);
-}
-
-// Process input from keyboard
-void processInput(GLFWwindow* window)
-{
-    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
-        glfwSetWindowShouldClose(window, true);
-}
 
 int main(void)
 {
+    /*glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);*/
+    //glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
     GLFWwindow* window;
 
     /* Initialize the library */
     if (!glfwInit())
         return -1;
 
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-    //glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // for Mac OS X
-
     /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(800, 600, "Hello World", NULL, NULL);
+    window = glfwCreateWindow(800, 600, "Application", NULL, NULL);
     if (!window)
     {
-        std::cout << "Failed to create GLFW window" << std::endl;
         glfwTerminate();
         return -1;
     }
@@ -42,28 +27,23 @@ int main(void)
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
 
-    /*set frambuffer function*/
-    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
-
-    // GLAD Loader
-    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    /*if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
     {
         std::cout << "Failed to initialize GLAD" << std::endl;
         return -1;
-    }
-
-    /*Set size of rendering window*/
-    glViewport(0, 0, 800, 600);
+    }*/
 
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
-
-        processInput(window);
-
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
 
+        glBegin(GL_TRIANGLES);
+        glVertex2f(-0.5f, -0.5f);
+        glVertex2f( 0.0f,  0.5f);
+        glVertex2f( 0.5f, -0.5f);
+        glEnd();
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
