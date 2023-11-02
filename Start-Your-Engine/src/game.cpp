6c0fd97a96b315ff9c0f955779668cc6603faee6
Kaih White <kaihgwhite@outlook.com>
2023-11-02 12:48:32 -0700

    updated utility code

---
+++ Start-Your-Engine/src/game.cpp
@@ -0,0 +1,35 @@
+/*
+Give credit to the author of LearnOpenGL.com Joey de Vries
+*/
+#include "game.h"
+
+Game::Game(unsigned int width, unsigned int height)
+    : State(GAME_ACTIVE), Keys(), Width(width), Height(height)
+{
+
+}
+
+Game::~Game()
+{
+
+}
+
+void Game::Init()
+{
+
+}
+
+void Game::Update(float dt)
+{
+
+}
+
+void Game::ProcessInput(float dt)
+{
+
+}
+
+void Game::Render()
+{
+
+}
--- Start-Your-Engine/src/game.h
+++ Start-Your-Engine/src/game.h
@@ -1,1 +1,35 @@
-#pragma once
+#ifndef GAME_H
+#define GAME_H
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+// Represents the current state of the game
+enum GameState {
+    GAME_ACTIVE,
+    GAME_MENU,
+    GAME_WIN
+};
+
+// Game holds all game-related state and functionality.
+// Combines all game-related data into a single class for
+// easy access to each of the components and manageability.
+class Game
+{
+public:
+    // game state
+    GameState               State;
+    bool                    Keys[1024];
+    unsigned int            Width, Height;
+    // constructor/destructor
+    Game(unsigned int width, unsigned int height);
+    ~Game();
+    // initialize game state (load all shaders/textures/levels)
+    void Init();
+    // game loop
+    void ProcessInput(float dt);
+    void Update(float dt);
+    void Render();
+};
+
+#endif
